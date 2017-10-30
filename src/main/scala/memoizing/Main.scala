b36b9a499808a0ac3a446d4d83facd38810f3935
Marcos Minond <minond.marcos@gmail.com>
2017-10-30 16:58:44 -0600

    logging in memoizing parser

--- src/main/scala/memoizing/Main.scala
+++ src/main/scala/memoizing/Main.scala
@@ -2,6 +2,8 @@ package memoizing
 
 object Main {
   def main(args: Array[String]) = {
+    println(s"Input: ${args(0)}")
+
     val lexer = new LookaheadLexer(args(0))
     val parser = new BacktrackParser(lexer)
 
